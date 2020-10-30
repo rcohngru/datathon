978291fa08a39dd3c3bf0826ef317f3a3651e8d9
Rafi <rafael.cohngruenwald@gmail.com>
2020-10-30 11:19:24 -0700

    pep8 styled

--- src/word_cloud.py
+++ src/word_cloud.py
@@ -2,8 +2,9 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
+
 def wordcloud_generator(corpus, stopwords=[]):
-    #combine specific cloud words 
+    # combine specific cloud words
     exclude_always = ['https', 'RT', 'co', 'tonight', 'flotus', 'amp']
     stopwords.extend(exclude_always)
     stopwords.extend(STOPWORDS)
@@ -14,13 +15,13 @@ def wordcloud_generator(corpus, stopwords=[]):
         tokens = val.split()
         for i in range(len(tokens)):
             tokens[i] = tokens[i].lower()
-                
+
         comment_words += " ".join(tokens) + " "
-    wordcloud = WordCloud(width = 800, height = 800, 
-                          background_color ='white', 
-                          stopwords = stopwords, 
-                          min_font_size = 10).generate(comment_words) 
-    plt.figure(figsize = (8, 8), facecolor = None) 
-    plt.imshow(wordcloud) 
-    plt.axis("off") 
-    plt.tight_layout(pad = 0) 
+    wordcloud = WordCloud(width=800, height=800,
+                          background_color='white',
+                          stopwords=stopwords,
+                          min_font_size=10).generate(comment_words)
+    plt.figure(figsize=(8, 8), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
